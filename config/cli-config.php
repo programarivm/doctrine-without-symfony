4f703976139f278877fd083ee41768de9b12ac01
programarivm <info@programarivm.com>
2018-06-19 21:02:01 +0100

    cli-config.php cleanup

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,27 +1,10 @@
 <?php
 
-use Doctrine\ORM\EntityManager;
+use AnotherBlog\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Doctrine\ORM\Tools\Setup;
-use Dotenv\Dotenv;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$dotenv = new Dotenv(__DIR__.'/../');
-$dotenv->load();
+$em = (new EntityManager())->get();
 
-$paths = array(__DIR__.'/../src/Entity');
-$isDevMode = true;
-
-$dbParams = array(
-    'host' => getenv('DB_HOST'),
-    'driver' => getenv('DB_DRIVER'),
-    'user' => getenv('DB_USERNAME'),
-    'password' => getenv('DB_PASSWORD'),
-    'dbname' => getenv('DB_DATABASE'),
-);
-
-$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, null, null, false);
-$entityManager = EntityManager::create($dbParams, $config);
-
-return ConsoleRunner::createHelperSet($entityManager);
+return ConsoleRunner::createHelperSet($em);
